df1d44fb2525a7b3eba7c4033c8d85e3e2ddae6a
TLosTT <93385875+TLosTT@users.noreply.github.com>
2021-12-03 13:24:13 +0300

    Add files via upload

---
+++ 5/script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector(".container");
+const preloader = document.querySelector(".preloader");
+
+const getData = async () => {
+  const response = await fetch("https://randomuser.me/api/?results=10", {
+    method: "GET",
+  });
+
+  const data = await response.json();
+
+  preloader.style.display = "none";
+
+  data.results.forEach((object1) => {
+    let card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.innerHTML = `
+                <div class="image">
+                <img src=${object1.picture.large}>
+                </div>
+                <div class="body">
+                    <p class="text">Фамилия и имя: ${object1.name.first} ${object1.name.last}</p>
+                    <p class="text">Пол: ${object1.gender}</p>
+                    <p class="text">Адрес: ${object1.location.country}, ${object1.location.state}, ${object1.location.city}, ${object1.location.street.name}, ${object1.location.street.number}</p>
+                    <p class="text">Email: ${object1.email}</p>
+                    <p class="text">Телефон: ${object1.phone}</p>
+                    <p class="text">Дата рождения: ${object1.dob.date.substring(0, 10).split("-").reverse().join("/")}</p>
+                    <p class="text">Возраст: ${object1.dob.age}</p>
+                </div>
+                    `;
+    container.appendChild(card);
+  });
+};
+
+window.onload = () => getData();
